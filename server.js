874ce54f6b6e2c666875fcf9283195ad22220e7a
shubhamimad <shubham_om@srmuniv.edu.in>
2016-10-05 21:41:41 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express');  //handling http connection
+var morgan = require('morgan');        //output logs
 var path = require('path');
 
 var app = express();
@@ -8,6 +8,27 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1',function(req,res){
+    
+    res.send("Araticle 1 requested will be saved here");
+    
+});
+
+
+app.get('/articl2',function(req,res){
+    
+    res.send("Araticle 2 requested will be saved here");
+    
+});
+
+
+app.get('/article3',function(req,res){
+    
+    res.send("Araticle 3 requested will be saved here");
+    
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
