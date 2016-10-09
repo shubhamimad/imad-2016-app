4336cd7726e4aa058223fa3518051f48b4959a64
shubhamimad <shubham_om@srmuniv.edu.in>
2016-10-09 01:45:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,13 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveright()
 {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft + 'px';
 }
 
 img.onclick = function()
 {
-    var interval=setInterval(moveright,100);
+    var interval=setInterval(moveright,500);
     
     
 };
